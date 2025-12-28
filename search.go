54e789789df1be829b878318541ed014b27b0828
iotwlw <iotwlw@126.com>
2025-12-28 22:33:11 +0800

    继续通过产品获取seller

--- search.go
+++ search.go
@@ -304,8 +304,7 @@ func (s *searchStruct) deal_prouct_url(link string, title string, boughtCount st
 	if strings.Contains(url[0], "/dp/") {
 		asin = strings.Split(url[0], "/dp/")[1]
 	}
-
-	_, err := app.db.Exec(`INSERT INTO product(url,param,title,asin,keyword,bought_count,price,rating,review_count,status) values(?,?,?,?,?,?,?,?,?)`, url[0], "/ref="+url[1], title, asin, s.en_key, boughtCount, price, rating, reviewCount, MYSQL_PRODUCT_STATUS_FROM_SEARCH)
+	_, err := app.db.Exec(`INSERT INTO product(url,param,title,asin,keyword,bought_count,price,rating,review_count) values(?,?,?,?,?,?,?,?,?)`, url[0], "/ref="+url[1], title, asin, s.en_key, boughtCount, price, rating, reviewCount)
 
 	link = fmt.Sprintf("https://%s%s", app.Domain, link)
 	if is_duplicate_entry(err) {
